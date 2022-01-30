92de21b005c23561c1226fef1f1582f5630e3c8f
Victor <vvillanuevaf@uni.pe>
2022-01-30 00:19:03 -0500

    agregando ninjas en /item

--- functions/api.js
+++ functions/api.js
@@ -6,6 +6,8 @@ const router = express.Router();
 app.use(cors())
 
 router.get('/', cors(), (req, res) => {
+
+
     res.json(
         [
             {
@@ -21,6 +23,30 @@ router.get('/', cors(), (req, res) => {
             {
                 'id': '003',
                 'name': 'lily Hoop',
+                'email': 'lily@gmail.com'
+            }
+        ]
+    )
+})
+
+router.get('/item', cors(), (req, res) => {
+
+    
+    res.json(
+        [
+            {
+                'id': '001',
+                'name': 'Gaara',
+                'email': 'smith@gmail.com'
+            },
+            {
+                'id': '002',
+                'name': 'Sasuke',
+                'email': 'sam@gmail.com'
+            },
+            {
+                'id': '003',
+                'name': 'Naruto',
                 'email': 'lily@gmail.com'
             }
         ]
